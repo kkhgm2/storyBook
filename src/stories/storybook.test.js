3510f306dd7ec28fd90893d1829b5d4a32a44ee8
kkhgm2 <pma225jkoi7265@yahoo.co.jp>
2021-03-20 21:25:38 +0900

    ビジュアル・リグレッションテストの追加

--- src/stories/storybook.test.js
+++ src/stories/storybook.test.js
@@ -1,2 +1,5 @@
 import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+initStoryshots({
+  test: imageSnapshot({ storybookUrl: 'http://localhost:6006/' }),
+});
